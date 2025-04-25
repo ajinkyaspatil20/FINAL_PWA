e8bf279e2b2b8055f884bb8c9d9afaf5b36c544e
ajinkyapatil2011 <ajinkyaspatil20@gmail.com>
2025-04-25 09:30:42 +0530

    search bar

--- maharashtra-forts-pwa/dev-dist/registerSW.js
+++ maharashtra-forts-pwa/dev-dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/FINAL_PWA/dev-sw.js?dev-sw', { scope: '/FINAL_PWA/', type: 'classic' })
--- maharashtra-forts-pwa/dev-dist/sw.js
+++ maharashtra-forts-pwa/dev-dist/sw.js
@@ -79,10 +79,10 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
+    "revision": "78e60a994fab888169179aa666a98a88"
   }, {
     "url": "index.html",
-    "revision": "0.uun4v16l9t"
+    "revision": "0.s4ilgke344g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
